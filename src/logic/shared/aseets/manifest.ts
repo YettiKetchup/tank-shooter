616c1735ec0c3d530d3f345d7121d52732e67dfb
YettiKetchup <yetti.ph@gmail.com>
2023-08-02 17:27:10 +0300

    - Remove junk.

--- src/logic/shared/aseets/manifest.ts
+++ src/logic/shared/aseets/manifest.ts
@@ -4,10 +4,6 @@ export const manifest = {
       name: 'game_screen',
       assets: [
         {
-          name: 'barrel_red.png',
-          srcs: '/assets/sprites/barrel_red.png',
-        },
-        {
           name: 'tank_spritesheet',
           srcs: '/assets/sprites/tank/tank.json',
         },
--- src/logic/stages/game-stage/views/root.view.ts
+++ src/logic/stages/game-stage/views/root.view.ts
@@ -1,10 +1,8 @@
 import { ViewBuilder } from 'mysh-pixi';
 import { Container } from 'pixijs';
-import { BarrelView } from './barrel.view';
 
 export const RootView = () => {
   //prettier-ignore
   return new ViewBuilder(Container)
-    .withNode(BarrelView())
   .build()
 };
