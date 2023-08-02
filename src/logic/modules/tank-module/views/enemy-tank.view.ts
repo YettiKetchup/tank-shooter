04e0382e32ac97111ce7869611c4ea1289515be6
YettiKetchup <yetti.ph@gmail.com>
2023-08-02 20:29:45 +0300

    - Enemy tank no longer an Entity.

--- src/logic/modules/tank-module/views/enemy-tank.view.ts
+++ src/logic/modules/tank-module/views/enemy-tank.view.ts
@@ -1,16 +1,9 @@
-import { ViewBuilder, EntityStorage } from 'mysh-pixi';
+import { ViewBuilder } from 'mysh-pixi';
 import { TankView } from './tank.view';
-import {
-  CanHoldComponent,
-  CursorTypeComponent,
-} from '@shared/interactive-module';
 
 export const EnemyTankView = () => {
   //prettier-ignore
   return new ViewBuilder(TankView('blue'))
     .inPosition(0, -300)
-    .asEntity(EntityStorage.get('game'))
-      .withComponent(new CanHoldComponent())
-      .withComponent(new CursorTypeComponent())
   .build();
 };
