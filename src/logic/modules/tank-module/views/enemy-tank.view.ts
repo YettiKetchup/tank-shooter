3bca7f21fda9864cd56fafbf610213d3ae11d5d4
YettiKetchup <yetti.ph@gmail.com>
2023-08-03 11:49:58 +0300

    - Tanks cleanup.

--- src/logic/modules/tank-module/views/enemy-tank.view.ts
+++ src/logic/modules/tank-module/views/enemy-tank.view.ts
@@ -1,54 +1,10 @@
-import { EntityStorage, ViewBuilder } from 'mysh-pixi';
+import { ViewBuilder } from 'mysh-pixi';
 import { TankView } from './tank.view';
 
-import {
-  ARPositionLandscape,
-  ARPositionPortrait,
-  ARScaleLandscape,
-  ARScalePortrait,
-  PivotLandscapeComponent,
-  PivotPortraitComponent,
-} from '@shared/resize-module';
-
 export const EnemyTankView = () => {
-  const relativePositionLandscape = new ARPositionLandscape({
-    yAboveAR: -0.8,
-    yBellowAR: -0.8,
-    xAboveAR: 0,
-    xBellowAR: 0,
-  });
-
-  const relativePositionPortrait = new ARPositionPortrait({
-    yAboveAR: -0.8,
-    yBellowAR: -0.8,
-    xAboveAR: 0,
-    xBellowAR: 0,
-  });
-
-  const scaleLandscape = new ARScaleLandscape({
-    yAboveAR: 1,
-    yBellowAR: 1.6,
-    xAboveAR: 1,
-    xBellowAR: 1.6,
-  });
-
-  const scalePortrait = new ARScalePortrait({
-    yAboveAR: 1.6,
-    yBellowAR: 1,
-    xAboveAR: 1.6,
-    xBellowAR: 1,
-  });
-
   //prettier-ignore
   return new ViewBuilder(TankView('blue'))
     .withPosition(0, -300)
-    .asEntity(EntityStorage.get('game'))
-      .withComponent(new PivotLandscapeComponent(0, 1, true))
-      .withComponent(new PivotPortraitComponent(0, 1, true))
-      .withComponent(relativePositionLandscape)
-      .withComponent(relativePositionPortrait)
-      .withComponent(scaleLandscape)
-      .withComponent(scalePortrait)
     .withAngle(180)
   .build();
 };
--- src/logic/modules/tank-module/views/player-tank.view.ts
+++ src/logic/modules/tank-module/views/player-tank.view.ts
@@ -2,53 +2,11 @@ import { ViewBuilder, EntitiesCollection } from 'mysh-pixi';
 import { TankView } from './tank.view';
 import { PlayerTankComponent } from '../components/player-tank.component';
 
-import {
-  ARPositionLandscape,
-  ARPositionPortrait,
-  ARScaleLandscape,
-  ARScalePortrait,
-  PivotLandscapeComponent,
-  PivotPortraitComponent,
-} from '@shared/resize-module';
-
 export const PlayerTankView = (collection: EntitiesCollection) => {
-  const positionLandscape = new ARPositionLandscape({
-    yAboveAR: 0.8,
-    yBellowAR: 0.8,
-    xAboveAR: 0,
-    xBellowAR: 0,
-  });
-
-  const positionPortrait = new ARPositionPortrait({
-    yAboveAR: 0.8,
-    yBellowAR: 0.8,
-    xAboveAR: 0,
-    xBellowAR: 0,
-  });
-
-  const scaleLandscape = new ARScaleLandscape({
-    yAboveAR: 1,
-    yBellowAR: 1.6,
-    xAboveAR: 1,
-    xBellowAR: 1.6,
-  });
-
-  const scalePortrait = new ARScalePortrait({
-    yAboveAR: 1.6,
-    yBellowAR: 1,
-    xAboveAR: 1.6,
-    xBellowAR: 1,
-  });
-
   //prettier-ignore
   return new ViewBuilder(TankView('red'))
+    .withPosition(0, 300)
     .asEntity(collection)
       .withComponent(new PlayerTankComponent())
-      .withComponent(new PivotLandscapeComponent(0, 1, true))
-      .withComponent(new PivotPortraitComponent(0, 1, true))
-      .withComponent(positionLandscape)
-      .withComponent(positionPortrait)
-      .withComponent(scaleLandscape)
-      .withComponent(scalePortrait)
   .build();
 };
